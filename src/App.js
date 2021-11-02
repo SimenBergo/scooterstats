d80015386d60f686d45f1e7cd05b5d8e37d3e6bd
Simen Bergo <bergo.simen@gmail.com>
2021-11-02 12:58:07 +0100

    logo stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import ScooterLogo from "../src/scooter512.png";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={ScooterLogo} alt="logo of a scooter"/>
         <h2>Scooter stats</h2>
       </header>
       <main>
