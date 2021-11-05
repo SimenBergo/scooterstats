b4d2b75f509e1170d3e340a53eff7f362af6e7b0
Simen Bergo <bergo.simen@gmail.com>
2021-11-05 13:39:25 +0100

    flipped structure so i can deploy

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const all = axios.create({
-    baseURL:  'http://localhost:5000/all',
+    baseURL:  'https://scooterstats.herokuapp.com/all',
 });
 
 export const getStats = () => all.get(`/getStats`);
